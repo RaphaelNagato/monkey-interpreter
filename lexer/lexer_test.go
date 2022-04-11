17756a628dcf1669fab95860638716a3ced506eb
Raphael Okeibunor <raphaelnagato19@gmail.com>
2022-04-11 01:36:34 +0100

    Add read number functionality in lexer

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `let five=5;
+	input := `let five = 5;
 	let ten = 10;
 	let add = fn(x,y) {
 		x + y;
@@ -25,6 +25,7 @@ func TestNextToken(t *testing.T) {
 		{token.SEMICOLON, ";"},
 		{token.LET, "let"},
 		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
 		{token.INT, "10"},
 		{token.SEMICOLON, ";"},
 		{token.LET, "let"},
